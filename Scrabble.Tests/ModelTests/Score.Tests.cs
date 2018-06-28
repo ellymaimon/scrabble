6c63384a5a8cbbed2381cd09330f4e8b178332f1
Elly Maimon and Kevin Ahn <elly@>
2018-06-28 09:57:35 -0700

    initial commit

---
+++ Scrabble.Tests/ModelTests/Score.Tests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Scrabble;
+
+namespace Scrabble.Tests
+{
+    [TestClass]
+    public class ScoreTest
+    {
+        [TestMethod]
+        public void DoIPass_TestToSeeIfThisWorks_True()
+        {
+            Score newScore = new Score();
+            Assert.AreEqual(,);
+        }
+    }
+}
